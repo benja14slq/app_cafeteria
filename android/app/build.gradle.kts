4e0d2ae0ec5acd63527edb7958e313e25b9fb709
benja14slq <benjamin.betty@sansano.usm.cl>
2025-05-23 18:42:48 -0400

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,6 @@ plugins {
 android {
     namespace = "com.example.app_cafeteria"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
     ndkVersion = "27.0.12077973"
 
     compileOptions {
